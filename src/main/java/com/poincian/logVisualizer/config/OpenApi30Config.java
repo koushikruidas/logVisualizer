313656503f98c1ab5d1c3c52286da94cfd18cb10
Koushik Ruidas <koushikruidas@gmail.com>
2025-08-31 12:34:39 +0530

    enabled swagger UI over api-gateway

--- src/main/java/com/poincian/logVisualizer/config/OpenApi30Config.java
+++ src/main/java/com/poincian/logVisualizer/config/OpenApi30Config.java
@@ -1,5 +1,6 @@
 package com.poincian.logVisualizer.config;
 
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -10,35 +11,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 
+import java.util.List;
+
 @Configuration
 public class OpenApi30Config {
 
-  private final String moduleName;
-  private final String apiVersion;
+    private final String moduleName;
+    private final String apiVersion;
 
-  public OpenApi30Config(
-      @Value("${module-name}") String moduleName,
-      @Value("${api-version}") String apiVersion) {
-    this.moduleName = moduleName;
-    this.apiVersion = apiVersion;
-  }
+    public OpenApi30Config(
+            @Value("${module-name}") String moduleName,
+            @Value("${api-version}") String apiVersion) {
+        this.moduleName = moduleName;
+        this.apiVersion = apiVersion;
+    }
 
-  @Bean
-  public OpenAPI customOpenAPI() {
-    final String securitySchemeName = "bearerAuth";
-    final String apiTitle = String.format("%s API", StringUtils.capitalize(moduleName));
-    return new OpenAPI()
-        .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-        .components(
-            new Components()
-                .addSecuritySchemes(securitySchemeName,
-                    new SecurityScheme()
-                        .name(securitySchemeName)
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        final String apiTitle = String.format("%s API", StringUtils.capitalize(moduleName));
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .servers(List.of(
+                        new Server().url("https://logpulse.dev/api/log-visualizer"),
+                        new Server().url("http://localhost:8080/log-visualizer")
+                ))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
                 )
-        )
-        .info(new Info().title(apiTitle).version(apiVersion));
-  }
+                .info(new Info().title(apiTitle).version(apiVersion));
+    }
 }
